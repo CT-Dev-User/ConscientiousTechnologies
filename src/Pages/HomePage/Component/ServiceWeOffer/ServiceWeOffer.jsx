e884c6fb2a3f613515c1e59aa7d9affdea5fe5b4
Chhatrapati Mandwe <mandwec12@gmail.com>
2024-09-25 12:11:39 +0530

    service we offer

--- src/Pages/HomePage/Component/ServiceWeOffer/ServiceWeOffer.jsx
+++ src/Pages/HomePage/Component/ServiceWeOffer/ServiceWeOffer.jsx
@@ -43,44 +43,43 @@ const ServiceWeOffer = () => {
     }, []);
   
     return (
-        <div id='service' className='w-[100vw] bg-[black] h-auto pt-[100px] md:pt-[40px] pb-[60px] md:pb-[0px] text-[white] ps-[110px] box-border md:ps-[0px]'>
-            <div className='md:w-[90%] md:mx-auto'>
-                <h1 className='text-3xl font-bold md:text-[18px]'>Service We Offers</h1>
-                <p className='w-[80%] md:w-[100%] text-[16px] md:text-[10px] mt-[10px]'>At CT, we are ready to offer end-to-end development that covers everything from business analysis to software delivery and support, help modernize your legacy software, or provide expert advisory at any stage of SDLC.. </p>
+        <div id='service' className='w-screen bg-[black] h-auto pt-24 md:pt-10 pb-16 md:pb-0 text-[white] box-border'>
+            <div className='w-[85%] md:w-[90%] mx-auto'>
+                <h1 className='text-3xl font-bold md:text-lg'>Service We Offers</h1>
+                <p className='w-full text-base md:text-[10px] mt-3 font-normal'>At CT, we are ready to offer end-to-end development that covers everything from business analysis to software delivery and support, help modernize your legacy software, or provide expert advisory at any stage of SDLC.. </p>
             </div>
-
-            <div className='w-[100%] flex gap-[30px] flex-wrap mt-[50px] md:hidden'>
+            <div className='w-[85%] md:w-[90%] mx-auto flex gap-x-6 gap-y-10 flex-wrap mt-10 md:hidden'>
                 {homeServiceData.map((item, index) => (
-                    <div key={index} className='service-item w-[20%]' onMouseEnter={() => setHoveredIndex(index)} onMouseLeave={() => setHoveredIndex(null)} onClick={() => handleServiceItemClick(index)}>
+                    <div key={index} className='service-item w-[23%]' onMouseEnter={() => setHoveredIndex(index)} onMouseLeave={() => setHoveredIndex(null)} onClick={() => handleServiceItemClick(index)}>
                        
-                        <div className='text-[white]  bg-red-500  w-[100%] h-[450px] relative text-[12px]' style={{ backgroundImage: `url(${item.ServiceHomePageimage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                        <div className='text-[white] bg-red-500 w-[100%] min-h-96 relative text-sm' style={{ backgroundImage: `url(${item.ServiceHomePageimage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
                             <div className='overlay'></div>
                             <div className={hoveredIndex === index ? 'w-[100%] h-[100%] absolute top-0 overlay show' : 'hidden'}>
-                                <div className=' p-4'>
+                                <div className='p-4'>
                                     {item.points.map((point, index) => (
                                         <li key={index} className="mb-2 list-none">
-                                            <h3 className="text-[14px] font-semibold underline ">{point.title}</h3>
-                                            <p className='font-normal text-[12px]' dangerouslySetInnerHTML={{ __html: point.description }} /> {/* Display description */}
+                                            <h3 className="text-sm font-semibold underline ">{point.title}</h3>
+                                            <p className='font-normal text-xs' dangerouslySetInnerHTML={{ __html: point.description }} /> {/* Display description */}
                                         </li>
                                     ))}
                                 </div>
                             </div>
-                            <button className='border border-[white] px-[20px] w-[150px] h-[45px] absolute bottom-[-10px] left-[-5px] text-center bg-cyan-500 cursor-pointer' style={{ opacity: hoveredIndex === index ? 1 : 0, transition: 'opacity .4s ease-in-out' }} onClick={() => { navigate(`/service/${item.title}`) }}>Explore More &rarr;</button>
-                            <button className='border border-[white] px-[20px] absolute bottom-[-10px] left-[-5px] bg-[#474747] w-[200px] h-[45px] text-center' style={{ opacity: hoveredIndex === index ? 0 : 1, transition: 'opacity .4s ease-in-out' }} onClick={() => { navigate(`/Services/${item.title}`) }}>{item.title}</button>
+                            <button className='text-xs border border-[white] px-5 w-40 h-11 absolute bottom-[-10px] left-[-5px] text-center bg-cyan-500 cursor-pointer' style={{ opacity: hoveredIndex === index ? 1 : 0, transition: 'opacity .4s ease-in-out' }} onClick={() => { navigate(`/service/${item.title}`) }}>Explore More &rarr;</button>
+                            <button className='text-xs border border-[white] px-4 absolute bottom-[-10px] left-[-5px] bg-[#474747] w-52 h-11 text-center' style={{ opacity: hoveredIndex === index ? 0 : 1, transition: 'opacity .4s ease-in-out' }} onClick={() => { navigate(`/Services/${item.title}`) }}>{item.title}</button>
                         </div>
                     </div>
                 ))}
             </div>
 
-            <div className='hidden md:flex w-[90%] flex-wrap mt-10 gap-[15px] mx-auto'>
+            <div className='hidden md:flex w-[90%] flex-wrap mt-10 gap-4 mx-auto'>
                 {homeServiceData.map((item, index) => (
-                    <div className='w-fit-content border border-gray-300 rounded-[25px] p-[5px]' key={index} onClick={() => handleServiceItemClick(index)}>
+                    <div className='w-fit-content border border-gray-300 rounded-6 p-1' key={index} onClick={() => handleServiceItemClick(index)}>
                         <h3 className='text-xs'>{item.title}</h3>
                     </div>
                 ))}
             </div>
 
-            <div className='hidden md:block w-[90%] h-[auto] mx-auto mt-[40px]'>
+            <div className='hidden md:block w-[90%] h-[auto] mx-auto mt-10'>
                 <Slider {...settings} className='w-[100%]' ref={sliderRef}>
                     {homeServiceData.map((item, index) => (
                         <div key={index} className='service-item'>
