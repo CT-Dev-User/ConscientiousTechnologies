21c4a6c64e1504e65ba2997eddd33964aa040408
Chhatrapati Mandwe <mandwec12@gmail.com>
2024-09-25 14:41:49 +0530

    taiilwind configuration changed

--- src/Pages/HomePage/Component/ServiceWeOffer/ServiceWeOffer.jsx
+++ src/Pages/HomePage/Component/ServiceWeOffer/ServiceWeOffer.jsx
@@ -1,102 +1,178 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 import Slider from "react-slick";
 import "./serviceWeOffer.css";
-import { useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const ServiceWeOffer = () => {
-    const [hoveredIndex, setHoveredIndex] = useState(null);
-    const [homeServiceData, setHomeServiceData] = useState([]);
-    const navigate = useNavigate()
-    const sliderRef = useRef(null);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [homeServiceData, setHomeServiceData] = useState([]);
+  const navigate = useNavigate();
+  const sliderRef = useRef(null);
 
-    const settings = {
-        dots: true,
-        dotsClass: "custom-dots",
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        speed: 700,
-        autoplaySpeed: 2500,
-        cssEase: "linear",
-        pauseOnHover: false,
-        arrows: false,
-    };
+  const settings = {
+    dots: true,
+    dotsClass: "custom-dots",
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 700,
+    autoplaySpeed: 2500,
+    cssEase: "linear",
+    pauseOnHover: false,
+    arrows: false,
+  };
 
-    const handleServiceItemClick = (index) => {
-        sliderRef.current.slickGoTo(index);
-    };
+  const handleServiceItemClick = (index) => {
+    sliderRef.current.slickGoTo(index);
+  };
 
-    const fetchHomeServiceData = async () => {
-        try {
-            const response = await axios.get("http://localhost:8080/get-service-data");
-            setHomeServiceData(response.data.getData);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  const fetchHomeServiceData = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/get-service-data"
+      );
+      setHomeServiceData(response.data.getData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    useEffect(() => {
-        fetchHomeServiceData();
-    }, []);
-  
-    return (
-        <div id='service' className='w-screen bg-[black] h-auto pt-24 md:pt-10 pb-16 md:pb-0 text-[white] box-border'>
-            <div className='w-[85%] md:w-[90%] mx-auto'>
-                <h1 className='text-3xl font-bold md:text-lg'>Service We Offers</h1>
-                <p className='w-full text-base md:text-[10px] mt-3 font-normal'>At CT, we are ready to offer end-to-end development that covers everything from business analysis to software delivery and support, help modernize your legacy software, or provide expert advisory at any stage of SDLC.. </p>
+  useEffect(() => {
+    fetchHomeServiceData();
+  }, []);
+
+  return (
+    <div
+      id="service"
+      className="w-screen bg-[black] h-auto pt-24 md:pt-10 pb-16 md:pb-0 text-[white] box-border"
+    >
+      <div className="w-[85%] md:w-[90%] mx-auto">
+        <h1 className="text-3xl font-bold md:text-lg 3xl:text-[2.5rem] 2xl:text-4xl">Service We Offers</h1>
+        <p className="w-full text-base md:text-[10px] mt-3 font-normal">
+          At CT, we are ready to offer end-to-end development that covers
+          everything from business analysis to software delivery and support,
+          help modernize your legacy software, or provide expert advisory at any
+          stage of SDLC..{" "}
+        </p>
+      </div>
+      <div className="w-[85%] md:w-[90%] mx-auto flex gap-x-6 gap-y-10 flex-wrap mt-10 md:hidden">
+        {homeServiceData.map((item, index) => (
+          <div
+            key={index}
+            className="service-item w-[23%]"
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
+            onClick={() => handleServiceItemClick(index)}
+          >
+            <div
+              className="text-[white] w-[100%] min-h-96 relative text-sm"
+              style={{
+                backgroundImage: `url(${item.ServiceHomePageimage})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <div className="overlay"></div>
+              <div
+                className={
+                  hoveredIndex === index
+                    ? "w-[100%] h-[100%] absolute top-0 overlay show"
+                    : "hidden"
+                }
+              >
+                <div className="p-4">
+                  {item.points.map((point, index) => (
+                    <li key={index} className="mb-2 list-none">
+                      <h3 className="text-sm font-semibold underline ">
+                        {point.title}
+                      </h3>
+                      <p
+                        className="font-normal text-xs"
+                        dangerouslySetInnerHTML={{ __html: point.description }}
+                      />{" "}
+                      {/* Display description */}
+                    </li>
+                  ))}
+                </div>
+              </div>
+              <button
+                className="text-xs border border-[white] px-5 w-40 h-11 absolute bottom-[-10px] left-[-5px] text-center bg-cyan-500 cursor-pointer"
+                style={{
+                  opacity: hoveredIndex === index ? 1 : 0,
+                  transition: "opacity .4s ease-in-out",
+                }}
+                onClick={() => {
+                  navigate(`/service/${item.title}`);
+                }}
+              >
+                Explore More &rarr;
+              </button>
+              <button
+                className="text-xs border border-[white] px-4 absolute bottom-[-10px] left-[-5px] bg-[#474747] w-52 h-11 text-center"
+                style={{
+                  opacity: hoveredIndex === index ? 0 : 1,
+                  transition: "opacity .4s ease-in-out",
+                }}
+                onClick={() => {
+                  navigate(`/Services/${item.title}`);
+                }}
+              >
+                {item.title}
+              </button>
             </div>
-            <div className='w-[85%] md:w-[90%] mx-auto flex gap-x-6 gap-y-10 flex-wrap mt-10 md:hidden'>
-                {homeServiceData.map((item, index) => (
-                    <div key={index} className='service-item w-[23%]' onMouseEnter={() => setHoveredIndex(index)} onMouseLeave={() => setHoveredIndex(null)} onClick={() => handleServiceItemClick(index)}>
-                       
-                        <div className='text-[white] bg-red-500 w-[100%] min-h-96 relative text-sm' style={{ backgroundImage: `url(${item.ServiceHomePageimage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                            <div className='overlay'></div>
-                            <div className={hoveredIndex === index ? 'w-[100%] h-[100%] absolute top-0 overlay show' : 'hidden'}>
-                                <div className='p-4'>
-                                    {item.points.map((point, index) => (
-                                        <li key={index} className="mb-2 list-none">
-                                            <h3 className="text-sm font-semibold underline ">{point.title}</h3>
-                                            <p className='font-normal text-xs' dangerouslySetInnerHTML={{ __html: point.description }} /> {/* Display description */}
-                                        </li>
-                                    ))}
-                                </div>
-                            </div>
-                            <button className='text-xs border border-[white] px-5 w-40 h-11 absolute bottom-[-10px] left-[-5px] text-center bg-cyan-500 cursor-pointer' style={{ opacity: hoveredIndex === index ? 1 : 0, transition: 'opacity .4s ease-in-out' }} onClick={() => { navigate(`/service/${item.title}`) }}>Explore More &rarr;</button>
-                            <button className='text-xs border border-[white] px-4 absolute bottom-[-10px] left-[-5px] bg-[#474747] w-52 h-11 text-center' style={{ opacity: hoveredIndex === index ? 0 : 1, transition: 'opacity .4s ease-in-out' }} onClick={() => { navigate(`/Services/${item.title}`) }}>{item.title}</button>
-                        </div>
-                    </div>
-                ))}
-            </div>
+          </div>
+        ))}
+      </div>
 
-            <div className='hidden md:flex w-[90%] flex-wrap mt-10 gap-4 mx-auto'>
-                {homeServiceData.map((item, index) => (
-                    <div className='w-fit-content border border-gray-300 rounded-6 p-1' key={index} onClick={() => handleServiceItemClick(index)}>
-                        <h3 className='text-xs'>{item.title}</h3>
-                    </div>
-                ))}
-            </div>
+      <div className="hidden md:flex w-[90%] flex-wrap mt-10 gap-4 mx-auto">
+        {homeServiceData.map((item, index) => (
+          <div
+            className="w-fit-content border border-gray-300 rounded-6 p-1"
+            key={index}
+            onClick={() => handleServiceItemClick(index)}
+          >
+            <h3 className="text-xs">{item.title}</h3>
+          </div>
+        ))}
+      </div>
 
-            <div className='hidden md:block w-[90%] h-[auto] mx-auto mt-10'>
-                <Slider {...settings} className='w-[100%]' ref={sliderRef}>
-                    {homeServiceData.map((item, index) => (
-                        <div key={index} className='service-item'>
-                            <div className='text-[white] w-[95%] h-[450px] bg-[white] relative text-[12px] gap-[10px]' style={{ backgroundImage: `url(${item.ServiceHomePageimage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                                <div className="overlay1"></div>
-                                <div className='w-[100%] h-[100%] absolute top-0'>
-                                    <div dangerouslySetInnerHTML={{ __html: item.points }} className='py-1 px-2' />
-                                </div>
-                                <button className='border border-[white] px-[20px] absolute bottom-0 bg-[#474747] w-[200px] h-[45px] text-center' onClick={() => { navigate(`/Services/${item.title}`) }}>{item.title}</button>
-                            </div>
-                        </div>
-                    ))}
-                </Slider>
+      <div className="hidden md:block w-[90%] h-[auto] mx-auto mt-10">
+        <Slider {...settings} className="w-[100%]" ref={sliderRef}>
+          {homeServiceData.map((item, index) => (
+            <div key={index} className="service-item">
+              <div
+                className="text-[white] w-[95%] h-[450px] bg-[white] relative text-[12px] gap-[10px]"
+                style={{
+                  backgroundImage: `url(${item.ServiceHomePageimage})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              >
+                <div className="overlay1"></div>
+                <div className="w-[100%] h-[100%] absolute top-0">
+                  <div
+                    dangerouslySetInnerHTML={{ __html: item.points }}
+                    className="py-1 px-2"
+                  />
+                </div>
+                <button
+                  className="border border-[white] px-[20px] absolute bottom-0 bg-[#474747] w-[200px] h-[45px] text-center"
+                  onClick={() => {
+                    navigate(`/Services/${item.title}`);
+                  }}
+                >
+                  {item.title}
+                </button>
+              </div>
             </div>
-
-        </div>
-    );
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceWeOffer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
-    screens: {
-      'sm': {'max': '639px'},
-      'md': {'max': '820px'},
-      'lg': {'max': '1023px'},
-      'xl': {'max': '1279px'}, 
-      '2xl':{'min':'1280px'},
-      '3xl': {'min': '1535px'}
-    }
+    screens: {},
   },
   plugins: [],
-}
+};
