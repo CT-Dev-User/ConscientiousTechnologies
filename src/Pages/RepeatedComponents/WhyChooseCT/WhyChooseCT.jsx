d93d9f2f2c681f43e1fb1717c387288acb4f63cd
Chhatrapati Mandwe <mandwec12@gmail.com>
2024-09-27 20:13:28 +0530

    solutions page

--- src/Pages/RepeatedComponents/WhyChooseCT/WhyChooseCT.jsx
+++ src/Pages/RepeatedComponents/WhyChooseCT/WhyChooseCT.jsx
@@ -1,152 +1,156 @@
-
 import React, { useEffect, useRef, useState } from "react";
 import Slider from "react-slick";
 import axios from "axios";
 
 const WhyChooseCTSlider = ({ category, subCategory }) => {
-    const sliderRef = useRef(null);
-    const [sliderData, setSliderData] = useState([])
-    const settings = {
-        dots: false,
-        infinite: true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: false, // Disable autoplay
-        speed: 2500,
-        autoplaySpeed: 0,
-        arrows: false,
-        cssEase: "linear",
-        responsive: [
-            {
-                breakpoint: 1440,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 3,
-                    infinite: true,
-                },
-            },
-            {
-                breakpoint: 1023,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                },
-            },
-            {
-                breakpoint: 900,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 2,
-                    infinite: true,
-                    autoplay: true,
-                },
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2,
-                    autoplay: true,
-                },
-            },
-            {
-                breakpoint: 500,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    autoplay: true,
-                },
-            },
-        ],
-    };
+  const sliderRef = useRef(null);
+  const [sliderData, setSliderData] = useState([]);
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: false, // Disable autoplay
+    speed: 2500,
+    autoplaySpeed: 0,
+    arrows: false,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          infinite: true,
+          autoplay: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+          autoplay: true,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          autoplay: true,
+        },
+      },
+    ],
+  };
 
-    const handlePrevSlide = () => {
-        sliderRef.current.slickPrev();
-    };
+  const handlePrevSlide = () => {
+    sliderRef.current.slickPrev();
+  };
 
-    const handleNextSlide = () => {
-        sliderRef.current.slickNext();
-    };
+  const handleNextSlide = () => {
+    sliderRef.current.slickNext();
+  };
 
-    const fetchSliderData = async () => {
-        try {
-            const response = await axios.get(`http://localhost:8080/get-choose-ct-slider-data-byCategorySubCategory/${category}/${subCategory}`);
-            setSliderData(response.data.data);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  const fetchSliderData = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/get-choose-ct-slider-data-byCategorySubCategory/${category}/${subCategory}`
+      );
+      setSliderData(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    useEffect(() => {
-        fetchSliderData()
-    }, [])
+  useEffect(() => {
+    fetchSliderData();
+  }, []);
 
-    return (
-        <div className="w-100 h-auto">
-            {
-                sliderData.map((slider, i) => {
-                    return (
-                        <div key={i} className="w-[100vw] h-[auto] mt-[100px] mb-[60px] md:mt-[30px]">
-                            <div className="w-[85%] mx-auto md:w-[100%] md:p-2">
-                                <h1 className="text-[30px] font-bold mt-[40px] mb-[20px] md:text-[24px]">
-                                    {slider.heading}</h1>
-                                <p>{slider.subtitle}</p>
-                            </div>
+  return (
+    <div className="w-screen h-auto">
+      {sliderData.map((slider, i) => {
+        return (
+          <div key={i} className="w-screen h-auto lg:mt-24 mb-14 mt-7">
+            <div className="lg:w-[85%] mx-auto w-full p-2 lg:p-0">
+              <h1 className="lg:text-3xl font-bold mt-10 mb-5 text-2xl">
+                {slider.heading}
+              </h1>
+              <p>{slider.subtitle}</p>
+            </div>
 
-                            <div className="w-[85%] mx-auto md:w-[90%]">
-                                <h1 className="text-[30px] font-bold mt-[40px] mb-[20px] md:text-[24px]">{slider.logoHeading}</h1>
+            <div className="lg:w-[85%] mx-auto w-[90%]">
+              <h1 className="lg:text-3xl font-bold mt-10 mb-5 text-2xl">
+                {slider.logoHeading}
+              </h1>
+            </div>
 
-                            </div>
+            <div className="flex items-center justify-between lg:mx-[110px] mx-5">
+              <button
+                onClick={handlePrevSlide}
+                className="btn-prev text-5xl font-normal hidden lg:block"
+              >
+                ‹
+              </button>
 
-                            <div className="flex items-center justify-between mx-[110px] md:mx-[20px]">
-                                <button onClick={handlePrevSlide} className="btn-prev text-[50px] font-normal md:hidden">
-                                    ‹
-                                </button>
-
-                                <div className="w-[75vw] text-[white] md:w-[85vw]">
-                                    <Slider ref={sliderRef} {...settings}>
-                                        {slider.logos.map((items, i) => (
-                                            <div  key={i}>
-                                                <div
-                                                    className="w-[200px] h-[100px] flex items-center md:w-[170px] md:h-[70px] md:ml-[10px]"
-                                                
-                                                >
-                                                    <div className="bg-[#D9D9D9] w-[70%] h-[100%] md:h-[80%] flex justify-center items-center text-[black]">
-                                                        <img src={items.logo} alt="fghjk" className="w-[100%] h-[100%]" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        ))}
-                                    </Slider>
-                                </div>
-
-                                <button onClick={handleNextSlide} className="btn-next text-[50px] font-normal md:hidden">
-                                    ›
-                                </button>
-                            </div>
-
-                            <div className="paretndiv w-[85%] mx-auto mt-[60px] md:w-[90%]">
-                                {
-                                    slider.points.map((point, i) => {
-                                        return (
-                                            <div className="flex gap-[10px]" key={i}>
-                                                <div className="w-[18px] h-[18px] bg-violet-300"></div>
-                                                <p className="text-[16px]">{point.title}</p>
-                                            </div>
-                                        )
-                                    })
-                                }
-
-                            </div>
+              <div className="lg:w-3/4 text-[white] w-[85%]">
+                <Slider ref={sliderRef} {...settings}>
+                  {slider.logos.map((items, i) => (
+                    <div key={i}>
+                      <div className="lg:w-[200px] lg:h-[100px] flex items-center w-[170px] h-[70px] ml-2">
+                        <div className="bg-[#D9D9D9] w-[70%] lg:h-full h-4/5 flex justify-center items-center text-black">
+                          <img
+                            src={items.logo}
+                            alt="fghjk"
+                            className="w-full h-full"
+                          />
                         </div>
-                    )
-                })
-            }
-        </div>
-    );
+                      </div>
+                    </div>
+                  ))}
+                </Slider>
+              </div>
+
+              <button
+                onClick={handleNextSlide}
+                className="btn-next text-5xl font-normal hidden lg:block"
+              >
+                ›
+              </button>
+            </div>
+
+            <div className="paretndiv lg:w-[85%] mx-auto mt-14 w-[90%]">
+              {slider.points.map((point, i) => {
+                return (
+                  <div className="flex gap-2 flex-wrap" key={i}>
+                    <div className="w-[18px] h-[18px] bg-violet-300"></div>
+                    <p className="text-base">{point.title}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default WhyChooseCTSlider;
-
---
+++ src/Pages/Solution/solutionPage/Solution.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import SolutionHeader from "./components/SolutionHeader";
+import OurPartener from "../../HomePage/Component/OurPartener/OurPartener";
+import WhyChooseCTSlider from "../../RepeatedComponents/WhyChooseCT/WhyChooseCT";
+import FunctionalERPModule from "../ERP/Components/FunctionalERPModule";
+import ERPDevService from "../ERP/Components/ERPDevService";
+import ERPDevSteps from "../ERP/Components/ERPDevSteps";
+import PerkOfERP from "../ERP/Components/PerkOfERP";
+import TechnologyHeading from "../ERP/Components/Technology/TechnologyHeading";
+import ReliableTools from "../../RepeatedComponents/ReliableTechTools/ReliableTechTools";
+import CaseStudies from "../../HomePage/Component/CaseStudies/CaseStudies";
+import MeetOurClient from "../../HomePage/Component/MeetOurClient/MeetOurClient";
+import AllFaq from "../../RepeatedComponents/AllFaq/AllFaq";
+import BookFreeConsultation from "../../HomePage/Component/BookFreeConsultation/BookFreeConsultation";
+import Footer from "../../HomePage/Component/Footer/Footer";
+
+const Solution = () => {
+  return (
+    <div>
+      <SolutionHeader />
+      <OurPartener />
+      <WhyChooseCTSlider category="Solutions" subCategory="ERP" />
+      <FunctionalERPModule />
+      <ERPDevService />
+      <ERPDevSteps/>
+      <PerkOfERP />
+      <TechnologyHeading />
+      <ReliableTools category="Solutions" subCategory="ERP" />
+      <CaseStudies category="Solutions" subCategory="ERP" />
+      <MeetOurClient />
+      <AllFaq category="Solutions" subCategory="ERP" />
+      <BookFreeConsultation />
+      <Footer />
+    </div>
+  );
+};
+
+export default Solution;
