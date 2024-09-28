8548004fca798c3565c2891863dc6d129872cd17
vaidyavaishali <vaishalivaidya1996@gmail.com>
2024-09-28 12:51:54 +0530

    soft dev steps

--- src/Pages/Services/SoftwareDevService/SoftDevHomePage/Component/SoftDevSteps.jsx
+++ src/Pages/Services/SoftwareDevService/SoftDevHomePage/Component/SoftDevSteps.jsx
@@ -78,12 +78,12 @@ const SoftDevSteps = () => {
     ]
     return (
         <div className='w-screen h-[auto]'>
-            <div className='w-[100vw] h-[auto] bg-[black] box-border pt-[60px] md:pt-[50px]  text-[white] pb-[60px] hidden lg:block'>
+            <div className='w-[100vw] h-[auto] bg-[black] box-border  py-16 text-[white] hidden lg:block'>
                 <div className='lg:w-[85%] mx-auto'>
                     <h1 className='lg:text-3xl text-xl 2xl:text-[2rem] font-bold text-[blue] lg:text-white'>Check Out Our Software Development Steps </h1>
                     <p className='w-[70%] lg:text-[14px] mt-2 md:w-[100%] text-[12px]'>Discover Our Software Development Steps: Gain insights into our comprehensive approach, from ideation to deployment, ensuring transparency and delivering high-quality, custom solutions for your unique business requirements.</p>
                 </div>
-                <div className="w-[85%] flex justify-between mx-[110px] mt-[60px]">
+                <div className="w-[85%] flex justify-between">
                     <div className="w-[47%] flex flex-col justify-start items-start gap-[230px] mt-[50px] ">
                         {processArr.map((step, index) => {
                             if (index % 2 == 0) {
@@ -133,7 +133,7 @@ const SoftDevSteps = () => {
                 </div>
                 <div className='w-[95%] flex mx-auto flex-wrap gap-3 mt-8 items-center justify-center '>
                     {processArr.map((item, index) => (
-                        <div key={index} className=' p-3 w-full h-auto border'>
+                        <div key={index} className='p-3 w-full h-auto border'>
                             <h1 className='text-[0.95rem]'>{item.title}</h1>
                             <div className='text-xs mt-2 font-medium'>{item.desc}</div>
                         </div>
