a7c61f1ad5cc2ceefc200c7718aec28241c17604
vaidyavaishali <vaishalivaidya1996@gmail.com>
2024-09-28 16:08:14 +0530

    update in area of experties

--- src/Pages/RepeatedComponents/AreaOfExperties/AreaOfExperties.jsx
+++ src/Pages/RepeatedComponents/AreaOfExperties/AreaOfExperties.jsx
@@ -22,7 +22,7 @@ const AreaOfExperties = ({ category, subCategory, navSubCategory }) => {
             <div className='w-[95%] mx-auto lg:w-4/5 flex flex-wrap gap-7  items-center justify-start'>
                 {expertiesData.map((items, i) => {
                     return (
-                        <div style={{ backgroundImage: `url(${items.image})`, backgroundSize: "cover", backgroundPosition: "center" }} className='lg:w-[22%] h-60 relative border w-full' key={i}>
+                        <div style={{ backgroundImage: `url(${items.image})`, backgroundSize: "cover", backgroundPosition: "center" }} className='lg:w-[22%] h-60 relative border w-[90%] md:w-[45%] mx-auto md:mx-0'  key={i}>
                             <div className='absolute top-0 left-0 w-full h-full'>
                                 <button className='text-white border border-[white] px-3 absolute bottom-[-10px] left-[-10px] bg-[#272727] h-auto text-left text-sm 2xl:text-base py-2' onClick={() => { navigate(`/${navSubCategory}/${items.title}`); }}>{items.title}</button>
                             </div>
